e3f27864138d86b3d190c8ff80433285fff09aac
Alina Komar <alinakomar11@gmail.com>
2026-03-25 13:32:29 +0300

    Add Pipeline items and configuration

---
+++ 2026_MT_Komar_A_A_Lab_!/Models/PipelineItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace _2026_MT_Komar_A_A_Lab__.Models
+{
+    public class PipelineItem
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("command")]
+        public string Command { get; set; } = string.Empty;
+
+        [JsonPropertyName("args")]
+        public string Args { get; set; } = string.Empty;
+
+        [JsonPropertyName("stopOnFailure")]
+        public bool StopOnFailure { get; set; } = true;
+
+        [JsonPropertyName("timeoutSeconds")]
+        public int TimeoutSeconds { get; set; } = 300;
+
+        [JsonPropertyName("workingDirectory")]
+        public string? WorkingDirectory { get; set; }
+
+        [JsonPropertyName("environment")]
+        public Dictionary<string, string>? Environment { get; set; }
+    }
+}
---
+++ 2026_MT_Komar_A_A_Lab_!/Models/PipelineSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace _2026_MT_Komar_A_A_Lab__.Models
+{
+    public class PipelineSettings
+    {
+        [JsonPropertyName("continueOnError")]
+        public bool ContinueOnError { get; set; } = false;
+
+        [JsonPropertyName("maxParallelStages")]
+        public int MaxParallelStages { get; set; } = 1;
+
+        [JsonPropertyName("artifactsPath")]
+        public string? ArtifactsPath { get; set; }
+
+    }
+}
