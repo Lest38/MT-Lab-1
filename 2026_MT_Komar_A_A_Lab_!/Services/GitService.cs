b045f0ec8deec0172d0d118e9badd290a5dbd451
Alina Komar <alinakomar11@gmail.com>
2026-03-27 12:35:05 +0300

    Add GitService with base git commands

---
+++ 2026_MT_Komar_A_A_Lab_!/Services/GitService.cs
@@ -0,0 +1,66 @@
+﻿using _2026_MT_Komar_A_A_Lab__.Models;
+using _2026_MT_Komar_A_A_Lab__.Services;
+using Microsoft.Extensions.Logging;
+
+namespace CICD.SyncVersion.Services;
+
+public class GitService
+{
+    private readonly ProcessRunner _processRunner;
+    private readonly ILogger<GitService> _logger;
+
+    public GitService(ProcessRunner processRunner, ILogger<GitService> logger)
+    {
+        _processRunner = processRunner;
+        _logger = logger;
+    }
+
+    public async Task<ProcessResult> CloneAsync(string targetDir, string repoUrl)
+    {
+        _logger.LogInformation("Cloning repository {RepoUrl} to {TargetDir}", repoUrl, targetDir);
+        return await _processRunner.RunCommandAsync(
+            "git",
+            $"clone {repoUrl} {targetDir}",
+            ".",
+            timeoutSeconds: 300
+        );
+    }
+
+    public async Task<ProcessResult> PullAsync(string targetDir)
+    {
+        _logger.LogInformation("Pulling latest changes in {TargetDir}", targetDir);
+        return await _processRunner.RunCommandAsync(
+            "git",
+            "pull",
+            targetDir,
+            timeoutSeconds: 60
+        );
+    }
+
+    public async Task<ProcessResult> GetCurrentBranchAsync(string targetDir)
+    {
+        return await _processRunner.RunCommandAsync(
+            "git",
+            "rev-parse --abbrev-ref HEAD",
+            targetDir
+        );
+    }
+
+    public async Task<ProcessResult> GetCommitHashAsync(string targetDir)
+    {
+        return await _processRunner.RunCommandAsync(
+            "git",
+            "rev-parse HEAD",
+            targetDir
+        );
+    }
+
+    public async Task<ProcessResult> GetStatusAsync(string targetDir)
+    {
+        return await _processRunner.RunCommandAsync(
+            "git",
+            "status --short",
+            targetDir
+        );
+    }
+}
