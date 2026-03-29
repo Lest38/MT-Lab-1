e736343453bda4ea1c5614f628dbe4acf4e754b7
Alina Komar <alinakomar11@gmail.com>
2026-03-29 17:35:44 +0300

    Add PipelineStats and ConfigurationService

---
+++ 2026_MT_Komar_A_A_Lab_!/Models/PipelineStats.cs
@@ -0,0 +1,18 @@
+﻿namespace _2026_MT_Komar_A_A_Lab__.Models
+{
+    public class PipelineStats
+    {
+        public int StageNumber { get; set; }
+        public int TotalStages { get; set; }
+        public int SuccessfulStages { get; set; }
+        public int FailedStages { get; set; }
+
+        public PipelineStats()
+        {
+            StageNumber = 0;
+            TotalStages = 0;
+            SuccessfulStages = 0;
+            FailedStages = 0;
+        }
+    }
+}
