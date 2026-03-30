2007881d19cd9d1ffacb0c81d715aa1362be3f4d
Alina Komar <alinakomar11@gmail.com>
2026-03-30 12:11:57 +0300

    Add logging

---
+++ 2026_MT_Komar_A_A_Lab_!/Utilities/FileLogger.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+
+public class FileLogger : ILogger
+{
+    private readonly string _filePath;
+    private static readonly object _lock = new();
+
+    public FileLogger(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        => default!;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log<TState>(
+        LogLevel logLevel,
+        EventId eventId,
+        TState state,
+        Exception? exception,
+        Func<TState, Exception?, string> formatter)
+    {
+        var message = formatter(state, exception);
+
+        lock (_lock)
+        {
+            File.AppendAllText(_filePath,
+                $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] [{logLevel}] {message}{Environment.NewLine}");
+        }
+    }
+}
---
+++ 2026_MT_Komar_A_A_Lab_!/Utilities/FileLoggerProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace _2026_MT_Komar_A_A_Lab__.Utilities
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        private readonly string _filePath;
+
+        public FileLoggerProvider(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+            => new FileLogger(_filePath);
+
+        public void Dispose() { }
+    }
+}
