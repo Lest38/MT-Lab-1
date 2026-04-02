99a83f606e40f07bb9346e9177cd70919255ba65
Alina Komar <alinakomar11@gmail.com>
2026-04-02 16:28:48 +0300

    Improve Filelogger

--- 2026_MT_Komar_A_A_Lab_!/Utilities/FileLogger.cs
+++ 2026_MT_Komar_A_A_Lab_!/Utilities/FileLogger.cs
@@ -7,11 +7,14 @@ namespace _2026_MT_Komar_A_A_Lab__.Utilities
     public class FileLogger : ILogger
     {
         private readonly string _filePath;
-        private static readonly object _lock = new();
+        private readonly object _lock = new();
 
         public FileLogger(string filePath)
         {
             _filePath = filePath;
+            var directory = Path.GetDirectoryName(filePath);
+            if (!string.IsNullOrEmpty(directory))
+                Directory.CreateDirectory(directory);
         }
 
         public IDisposable? BeginScope<TState>(TState state) where TState : notnull
@@ -27,11 +30,16 @@ namespace _2026_MT_Komar_A_A_Lab__.Utilities
             Func<TState, Exception?, string> formatter)
         {
             var message = formatter(state, exception);
+            var logEntry = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] [{logLevel}] {message}";
+
+            if (exception != null)
+            {
+                logEntry += $"{Environment.NewLine}{exception}";
+            }
 
             lock (_lock)
             {
-                File.AppendAllText(_filePath,
-                    $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] [{logLevel}] {message}{Environment.NewLine}");
+                File.AppendAllText(_filePath, logEntry + Environment.NewLine);
             }
         }
     }
